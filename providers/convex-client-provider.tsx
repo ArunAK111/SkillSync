11cf9d7356488a92bbbc327c759c33874f47b9c1
ArunAK111 <arunkurali9@gmail.com>
2024-05-11 19:55:53 +0530

    provider

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -9,3 +9,24 @@ interface ConvexClientProviderProps {
     children: React.ReactNode;
 }
 
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider: React.FC<ConvexClientProviderProps> = ({ children }) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                <Unauthenticated>
+                    {children}
+                </Unauthenticated>
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <AuthLoading>
+                    <Loading />
+                </AuthLoading>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
